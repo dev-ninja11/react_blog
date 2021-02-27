b55e5f808565c56612459d3017ad2756a9fc101e
dev-ninja11 <ninjadev1010@gmail.com>
2021-02-26 23:29:26 -0600

    Created the PostApi.js
    This API imports the Base API, and it connects to the Post API.  This class class component  API creates objects to make the CRUD operations in the Backend

---
+++ blog-project/src/API/PostApi.js
@@ -0,0 +1,36 @@
+
+import Axios from './BaseApi'
+
+class PostApi{
+    static getAllPosts(){//gets all the posts from the API
+        return Axios.get(`/posts/`)
+    }
+
+    static getPost(post_url){
+        return Axios.get(`posts/$(jsonplaceholder.typicode.com/posts)`)
+    }
+
+    static newPost(new_post_object){
+        return Axios.post(`posts`,new_post_object)
+    }
+
+    static getCommentforPost(post_url){
+        return Axios.get(`/postsjsonplaceholder.typicode.com/posts/comments`)
+    }
+
+    static updatePost(post_url, updated_post){
+        return Axios.put(`/post/jsonplaceholder.typicode.com/posts`,updated_post)
+    }
+
+    static patchPost(post_url, updated_fields){//updates a part of the post API
+        return Axios.patch(`/posts/jsonplaceholder.typicode.com/posts`,updated_fields)
+    }
+
+    static deletePost(post_url){//deletes a post in the API
+        return Axios.deletes(`/posts/jsonplaceholder.typicode.com/posts`)
+    }
+
+    
+}
+
+export default PostApi
